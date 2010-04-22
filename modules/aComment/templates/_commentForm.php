94199891395245e1c2fbe647345c93fabd3947ab
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-22 19:37:56 +0000

    Change to css id of comment form
    git-svn-id: http://svn.apostrophenow.org/plugins/sfDoctrineActAsCommentablePlugin/trunk@1014 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aComment/templates/_commentForm.php
+++ modules/aComment/templates/_commentForm.php
@@ -1,4 +1,4 @@
-<div id="<?php echo get_class($object).'-'.$object['id'] ?>comment-form">
+<div id="<?php echo get_class($object).'-'.$object['id'] ?>-comment-form">
 
 <?php include_partial('aComment/form', array('form' => $form, 'object' => $object)) ?>
 
